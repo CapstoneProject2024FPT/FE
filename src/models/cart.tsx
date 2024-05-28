cca7f57911b0bcf8e6e9fb423decd0564f41d7de
holeshit1234 <hominhdund@gmail.com>
2024-05-28 09:03:46 +0700

    cart

---
+++ src/models/cart.tsx
@@ -0,0 +1,11 @@
+interface cartProps {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  brandName: string;
+  automatic: boolean;
+  image: File;
+}
+
+export default cartProps;
