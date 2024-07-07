4522b07156aaa8496aac3d195f933443a6df0e9b
Nguyen Tien Tai <capterfinn123@gmail.com>
2024-07-07 15:47:58 +0700

    fix order detail

--- src/models/order.tsx
+++ src/models/order.tsx
@@ -9,14 +9,38 @@ export interface OrderProps {
     note: string;
     status: string;
     userInfo: UserInfo;
-    address: string;
+    address: AddressDetail
 }
 
+interface AddressDetail {
+    name: string;
+    status: string;
+    note: string;
+    city: CityDetail[];
+    district: DistrictDetail[];
+    ward: WardDetail[];
+}
 interface ProductDetail {
     productId: string;
     productName: string;
     quantity: number;
     totalAmount: number;
+}
+
+interface CityDetail {
+    id: string;
+    unitId: number;
+    name: string;
+}
+interface DistrictDetail {
+    id: string;
+    unitId: number;
+    name: string;
+}
+interface WardDetail {
+    id: string;
+    unitId: number;
+    name: string;
 }
 
 interface UserInfo {
