3394cc9a3d7f035e552c8c0f69b8f85e0ca1dfde
holeshit1234 <hominhdund@gmail.com>
2024-08-07 08:00:14 +0700

    Fix: login string

--- src/pages/Login/SignIn.tsx
+++ src/pages/Login/SignIn.tsx
@@ -25,14 +25,8 @@ const SignInForm: React.FC = () => {
   const { apiLogin } = AuthApi();
   const { apiUserProfile } = CustomerApi();
   const LoginSchema = Yup.object().shape({
-    username: Yup.string()
-      .required("bắt buộc")
-      .min(5, "Tối thiểu 5 kí tự")
-      .trim(),
-    password: Yup.string()
-      .required("bắt buộc")
-      .min(8, "Tối thiểu 8 kí tự")
-      .trim(),
+    username: Yup.string().required("bắt buộc").trim(),
+    password: Yup.string().required("bắt buộc").trim(),
   });
 
   const role = ["Sale", "Admin", "Manager"];
