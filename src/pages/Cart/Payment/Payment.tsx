cad8920d10cb6b33b7e6a7774ad43a2772fa2ef1
holeshit1234 <hominhdund@gmail.com>
2024-08-09 17:07:52 +0700

    Fix: dashboard rechart

--- src/pages/Cart/Payment/Payment.tsx
+++ src/pages/Cart/Payment/Payment.tsx
@@ -111,7 +111,7 @@ const CheckoutPayment: React.FC<checkoutPaymentProps> = ({
             //handle send email
             isSucess = true;
             // TODO:
-            // handleSendEmail(isSucess, email, username);
+            handleSendEmail(isSucess, email, username);
           }
         } catch (error) {
           console.error("Error updating payment status:", error);
